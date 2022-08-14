5bae7b4e2a13fd1faf0fd90cd6e448bdd8ac8414
Devon <warshaw.devon@gmail.com>
2022-08-14 01:17:32 -0700

    switch to secrets

--- missingCardFinder.py
+++ missingCardFinder.py
@@ -1,18 +1,6 @@
 import os
 import io
 
-sets = {"AOTC": 180, "SR": 90, "ANH": "A New Hope", "BOY": "Battle of Yavin", "JG": "Jedi Guardians", "ESB": "The Empire Strikes Back",
-"RAS": "Rogues and Scoundrels", "PM": "The Phantom Menace", "ROTJ": "Return of the Jedi", "ROTS": "Revenge of the Sith", "FOTR": 120,
-"SAV": 90, "BOE": 180, "RAW": 120, "ION": 180, "BOH": 60, "BH": 75, "MAND": 75, "SITH": "Legacy of the Force: Sith",
-"SMUG": "Legacy of the Force: Smugglers", "JEDI": "Legacy of the Force: Jedi", "RO2": "Rule of Two",
-"TOR": "The Old Republic", "CWSO": "The Clone Wars: Separatist Offensive", "RS": "Rogue Squadron", "ER": "Empire Rising", "EE": "Empire Eternal", "AGD": "The Clone Wars: A Galaxy Divided",
-"TDT": "The Dark Times", "CAD": "Clones and Droids", "TFA": "The Force Awakens", "VP": "The New Jedi Order: Vector Prime", "TAL": "The Old Republic: Tales and Legends",
-"SBS": "The New Jedi Order: Star by Star", "DAN": "The Old Republic: Days and Nights", "BL": "The Clone Wars: Battle Lines", "RO": "Rogue One",
-"TEN": "Tenth Anniversary", "BOSB": "Battle of Starkiller Base", "TLJ": "The Last Jedi", "SOR": "Spark of Rebellion", "BF": "Battlefront", "JK": "Jedi Knight",
-"TUF": "The New Jedi Order: The Unifying Force", "BOC": "Battle of Crait", "SOLO": "Solo: A Star Wars Story", "KAE": "The Old Republic: Knights and Exiles",
-"TM": "The Mandalorian", "15TH": "15th Anniversary", "TROS": "The Rise of Skywalker", "AAA": "Apprentices and Assassins", "FOR": "Fires of Rebellion", 
-"BOTS": "Battle of the Sarlacc", "TMW": "The Mandalorian Way", "BOBF": "The Book of Boba Fett"}
-
 baseSetPath = "starwars/sets/"
 
 missingCards = {}
@@ -30,7 +18,7 @@ def processULCard(imageFrag, currentSet, cards):
 
 	slicedFrag = imageFrag[:-4]
 	if slicedFrag not in cards.keys():
-		missingCards[slicedFrag] = slicedFrag + " was an updatelist.txt entry but wasn't found in " + currentSet + ".txt"
+		missingCards[slicedFrag] = slicedFrag + " has an updatelist.txt entry but wasn't found in " + currentSet + ".txt"
 	else:
 		cards[slicedFrag] = False
 	
@@ -74,7 +62,7 @@ def processUpdateList():
 			if nextSet != currentSet:
 				for key in cards.keys():
 					if cards[key] == True:
-						missingCards[key] = key + " from set file " + currentSet + ".txt has no corresponding updateList entry"
+						missingCards[key] = key + " from set file " + currentSet + ".txt has no corresponding updatelist.txt entry"
 				currentSet = nextSet
 				cards = processSetFile(currentSet)
 			processULCard(imageLink, currentSet, cards)
